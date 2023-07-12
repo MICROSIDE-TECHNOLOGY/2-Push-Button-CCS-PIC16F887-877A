f4a27e48f369246aafcf8724099175af839f072f
vhperaltaj <victor.peralta@microside.com>
2023-07-12 17:13:01 -0600

    Initial commit

---
+++ Proyecto/PIC16F887/Push Button.c
@@ -0,0 +1,45 @@
+/*
+   AUTOR: MICROSIDE TECHNOLOGY S.A. DE C.V.
+   FECHA: JUNIO 2019
+*/
+
+/*
+---------------------------------------------------------------------------
+ Implementar el control de encendido y apagado de un LED mediante
+ un botón pulsador.
+---------------------------------------------------------------------------
+*/
+
+#include <16F887.h>                            //Incluye el microcontrolador con el que se va a trabajar 
+#use delay(clock=20Mhz, crystal)                //Tipo de oscilador y frecuencia dependiendo del microcontrolador 
+
+#define LED PIN_A1                              //Asigna el pin A1 al LED del X-TRAINER
+#define BOTON PIN_A2                            //Asigna el pin A2 al boton BOOT del X-TRAINER
+
+void SisInit (void)
+
+ {
+     set_tris_a (0b11111101);                   //Pin A1 como salida
+ }
+
+void main (void)
+ {
+   SisInit ();
+
+   while (1)                                     //Ciclo repetitivo
+   
+   {
+    if (0 == input (BOTON))                      //Lee el estado del boton
+      {
+       output_high (LED);                        //Si se presiona el boton enciende el LED
+      }
+
+     else
+
+     {
+      output_low (LED);                          //De lo contrario el LED se apaga
+     }
+
+   }
+
+}
